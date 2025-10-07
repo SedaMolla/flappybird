acb6c3c9a4bfeb32eb86cf4744614e406c26ad82
SedaMolla <232644266+SedaMolla@users.noreply.github.com>
2025-10-07 19:36:19 +0200

    week-03: draw static bird and pipes

--- src/flappybird.js
+++ src/flappybird.js
@@ -1,62 +1,61 @@
-// Tahta (canvas)
+// Board (canvas)
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
-// Kuş
+
+// Bird
 let birdWidth = 34;
 let birdHeight = 24;
 let birdX = boardWidth / 8;
 let birdY = boardHeight / 2;
 let birdImg;
 
-// Borular
+// Pipes
 let pipeWidth = 64;
 let pipeHeight = 512;
 let pipeX = boardWidth;
 let pipeY = 0;
 
-let topPipeImg;
-let bottomPipeImg;
+let toppipeImg;
+let bottompipeImg;
 
 window.onload = function () {
-  // Canvas'ı bul ve ayarla
+  // Find and set up the canvas
   board = document.getElementById("board");
   board.width = boardWidth;
   board.height = boardHeight;
   context = board.getContext("2d");
 
-  // Arka plan testi (opsiyonel)
+  // Background test (optional)
   // context.fillStyle = "green";
   // context.fillRect(100, 100, 30, 50);
 
-  // Görselleri yükle
+  // Load images
   birdImg = new Image();
   birdImg.src = "./flappybird.png";
-  birdImg.onload = function () {
-    // Kuşu çiz
+  birdImg.onload = function () 
+  {
+    // Draw the bird
     context.drawImage(birdImg, birdX, birdY, birdWidth, birdHeight);
   };
 
-  topPipeImg = new Image();
-  topPipeImg.src = "./toppipe.png";
-  topPipeImg.onload = function () {
-    // Üst boru
-    context.drawImage(topPipeImg, pipeX, pipeY, pipeWidth, pipeHeight);
+  toppipeImg = new Image();
+  toppipeImg.src = "./toppipe.png";
+  toppipeImg.onload = function () 
+  {
+    // Draw the top pipe
+    context.drawImage(toppipeImg, pipeX, pipeY, pipeWidth, pipeHeight);
   };
 
-  bottomPipeImg = new Image();
-  bottomPipeImg.src = "./bottompipe.png";
-  bottomPipeImg.onload = function () {
-    // Alt boru (biraz aşağıda)
-    let gap = 150; // kuşun geçeceği boşluk
-    context.drawImage(
-      bottomPipeImg,
-      pipeX,
-      pipeY + pipeHeight + gap,
-      pipeWidth,
-      pipeHeight
-    );
+  bottompipeImg = new Image();
+  bottompipeImg.src = "./bottompipe.png";
+  bottompipeImg.onload = function () 
+  {
+    // Draw the bottom pipe (slightly lower)
+    let gap = 150; // space for the bird to pass through
+    context.drawImage( bottompipeImg, pipeX, pipeY + pipeHeight + gap, pipeWidth, pipeHeight);
   };
 };
+
